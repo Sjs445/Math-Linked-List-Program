5eac451bd471234fdbcba67d541f95c1bfea5d16
Sjs445 <sjs445@csu.fullerton.edu>
2019-04-23 20:27:07 -0700

    Fixed input of operator and handling memory leaks.

--- calculator.cpp
+++ calculator.cpp
@@ -40,22 +40,22 @@ int main() {
     ofstream outFile ("output.txt");
     char anOperator;
     while (!inFile.eof()){
-        // implement program logic here
         left=loadNumber(inFile);
         right=loadNumber(inFile);
         anOperator=getOperator(inFile);
         if(anOperator=='+')
         {
             result=addNumbers(left, right);
+            writeNumRecurse(result, outFile);
         }
     }
-    writeNumRecurse(result, outFile);
+
     outFile.close();
     inFile.close();
 deleteNumber(left);
 deleteNumber(right);
 deleteNumber(result);
-cout<<"test";
+
     return 0;
 }
 
@@ -99,12 +99,12 @@ digit * loadNumber(ifstream & file){
 
 while(file.get(number))  //while we can get the character in the text file...
 {
-  if(number=='\n')   //If the number in the text file is a new line then break from the while loop.
+  if(number=='\n')   //If the number in the text file is a new line then break.
   break;
-  num=(int)number-'0';  //Casts the character from ASCII to int by subtracting the '0' character decimal value from the character value
-                            // inside number.
-                            //EX: Our first number is 1...in ASCII '1' is 49 as a decimal. Since '0' in ASCII is 48 as decimal...49-48=1.
-                            //This should work for all numbers from 0-9.
+  num=(int)number-'0';  //Casts the character from ASCII to int by subtracting
+      //the '0' character decimal value from the character value inside number.
+      //EX: Our first number is 1...in ASCII '1' is 49 as a decimal. Since '0'
+      //in ASCII is 48 as decimal...49-48=1...Then '2' is 50 so 50-48=2. So on...
 
   digit * node = new digit;   //Creating the linked list node here.
   node->data=num; //set the data value for the newly created node as num.
@@ -117,21 +117,23 @@ while(file.get(number))  //while we can get the character in the text file...
 
 char getOperator(ifstream & file){
 char oper;
+string line;
 file.get(oper);
+getline(file, line); //get the rest of the line
 return oper;
 }
 
 void printNumrecurse(digit * num){
-if(num==nullptr)
+if(num==nullptr)  //Base Case: If our node is null, end the function.
 {
   cout<<endl;
   return;
 }
-else
+else      //Otherwise print the data value in the node.
 {
   cout<<num->data;
-  num=num->next;
-  printNumrecurse(num);
+  num=num->next;  //go to the next node.
+  printNumrecurse(num); //call the function again.
 }
 }
 //The print numrecurse does the same as printNum...
@@ -143,9 +145,12 @@ void printNum(digit * num){
     }
 }
 
+//This function is the same as printNumrecurse except it writes the numbers
+//instead of printing them.
 void writeNumRecurse(digit * num, ofstream & file) {
 if(num==NULL)
 {
+  file<<endl;
   return;
 }
 else
@@ -157,18 +162,17 @@ else
 }
 
 void writeNumber(digit * num, ofstream & file){
-
     return;
 }
 
+//While our pointer is not NULL we delete the node.
+//We also could've done this recursively.
 void deleteNumber(digit * num){
-  while(num->next!=NULL)
+  while(num!=NULL)
   {
     delete num;
-    num->next=NULL;
     num=num->next;
   }
-  num=NULL;
     return;
 }
 
@@ -177,10 +181,11 @@ digit * addNumbers(digit * left, digit * right){
   digit * head=nullptr;
 
 sizedifference=getSizeDifference(left, right);
+//Gets the difference in size of the two linked lists.
 if(getSize(left) < getSize(right))
-{
+{ //If the first linked list is smaller than the second...
   for(int i=0; i<sizedifference; i++)
-  {
+  { //We add a however many 0s we need to make the two linked lists the same size.
       left=addTail(left, 0);
   }
 }
@@ -191,27 +196,32 @@ else if(getSize(right)<getSize(left))
     right=addTail(right, 0);
   }
 }
-
+//While we haven't reached the end of both of the linked lists...
   while(left!=NULL && right!=NULL)
   {
     digit * newnode=new digit;  //create a new linked list which will contain
                                 //the result of adding the two numbers.
     if(((left->data)+(right->data))>9)
-    {
+    { //If the two numbers needed to be added are greater than 9..Carry
+      //the number.
       result=addCarry(left, right, carry);
-      carry=(left->data+right->data)/10;
+      carry=(left->data+right->data)/10;  //We need to carry the remainder to
+                                          //the next node.
       head=addNode(result, head);
     }
     else
     {
+      //If the two numbers don't need to carry, add it to the linked list.
       result=left->data+right->data+carry;
       head=addNode(result, head);
-      carry=0;
+      carry=0;  //Important to reset carry to 0 because we don't carry
+                //another number in this case.
     }
     left=left->next;
     right=right->next;
   }
 
+//Here we are checking if we need to carry the LAST number.
   if(carry==0)
   {
     return head;
@@ -223,6 +233,7 @@ else if(getSize(right)<getSize(left))
   }
 }
 
+//Gets the size of the linked list.
 int getSize(digit * head)
 {
   int count=0;
@@ -234,17 +245,18 @@ int getSize(digit * head)
   return count;
 }
 
+//Adding a node to the tail.
 digit * addTail(digit * head, int num)
 {
-  digit * node = new digit;
-  node->data=num;
-  digit * tail=head;
-  if(head==NULL)
+  digit * node = new digit; //Declare a new node.
+  node->data=num;         //Set the data field of the new node to num.
+  digit * tail=head;  //Declare a new pointer and set it to the head of the list.
+  if(head==NULL)  //If the linked list is empty just add the new node.
   {
     head=node;
   }
   else
-  {
+  { //Otherwise loop to the end of the linked list and add the node to the end.
     while(tail->next!=NULL)
     {
       tail=tail->next;
@@ -254,6 +266,7 @@ digit * addTail(digit * head, int num)
   return head;
 }
 
+//Returns the difference in size between the two linked lists.
 int getSizeDifference(digit * left, digit * right)
 {
   int size1=0, size2=0;
@@ -281,8 +294,7 @@ int getSizeDifference(digit * left, digit * right)
   }
 }
 
-
-
+//Adds the node to the head of the linked list.
 digit * addNode(int num, digit * head)
 {
   digit * node=new digit;
@@ -295,7 +307,7 @@ digit * addNode(int num, digit * head)
 int addCarry(digit * left, digit * right, int carry)
 {
   int result=left->data+right->data+carry;
-  result=result-10;
+  result=result-10; //Our result needs to be 1 number.
   return result;
 }
 
